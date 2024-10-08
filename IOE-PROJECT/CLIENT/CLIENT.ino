271be10b2ede90df926ca7fa1954383bdc186fd5
ᴀᴍᴍᴀʀ ɴᴀɢᴀʀᴊɪ <ammar@nagarji.in>
2024-10-08 13:52:39 +0530

    Final Update

--- IOE-PROJECT/CLIENT/CLIENT.ino
+++ IOE-PROJECT/CLIENT/CLIENT.ino
@@ -1,25 +1,36 @@
-#include <SoftwareSerial.h>
 #include <string.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-SoftwareSerial mySerial(RX, TX);
+const char *ssid =  "Friday";
+const char *pass =  "12345678";
 
 long duration;
-int distance[5], minDistance, checkDuration = 5;
+int distance[5], minDistance = 100, checkDuration = 5;
 int trigPin[5], echoPin[5];
-int carStill[5];
-int shareFlag = 0;
+int carStill[5], jamCounter[5];
+int shareFlag;
+String jC;
+
+String server = "http://iracloud.yantravyuh.com/smartSignal/";
+String update_density = server + "update_density.php?shareFlag=" + shareFlag;
+String update_jam_count = server + "update_jam_count.php?jC=" + jC;
+
+
+WiFiClient client;
+HTTPClient http;
 
 void setup() {
   // put your setup code here, to run once:
 
-  trigPin[0] = 16;
-  echoPin[0] = 5;
-  trigPin[1] = 4;
-  echoPin[1] = 0;
-  trigPin[2] = 14;
-  echoPin[2] = 12;
-  trigPin[3] = 13;
-  echoPin[3] = 15;
+  trigPin[0] = 16; //D0
+  echoPin[0] = 5; //D1
+  trigPin[1] = 4; //D2
+  echoPin[1] = 0; //D3
+  trigPin[2] = 14; //D5
+  echoPin[2] = 12; //D6
+  trigPin[3] = 15; //D8
+  echoPin[3] = 13; //D7
 
   pinMode(trigPin[0], OUTPUT);
   pinMode(echoPin[0], INPUT);
@@ -29,14 +40,31 @@ void setup() {
   pinMode(echoPin[2], INPUT);
   pinMode(trigPin[3], OUTPUT);
   pinMode(echoPin[3], INPUT);
-  Serial.begin(115200);
-  mySerial.begin(9600);
 
+  //  pinMode(D0, OUTPUT);
+  //  pinMode(D1, INPUT);
+  Serial.begin(115200);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
   Serial.println("Transmitter Ready");
 }
 
 void loop() {
+  shareFlag = 0;
   // put your main code here, to run repeatedly:
+  Serial.println("Loop!!");
+  delay(2000);
   for (int i = 0; i < 4; i++) {
     // Clears the trigPin
     digitalWrite(trigPin[i], LOW);
@@ -53,22 +81,38 @@ void loop() {
     // Calculating the distance
     distance[i] = duration * 0.034 / 2;
     // Prints the distance on the Serial Monitor
-    Serial.print("Distance: ");
+    Serial.print("Distance ");
+    Serial.print(i);
     Serial.println(distance[i]);
     if (distance[i] <= minDistance) {
       carStill[i] = checkTraffic(i);
     }
+    else
+      carStill[i] = 2;
     shareFlag *= 10;
-    shareFlag += carStill[i]; 
+    shareFlag += carStill[i];
     delay(1000);
   }
-  mySerial.println(shareFlag);  // Send message
-  Serial.println("Message sent");
+  Serial.println(shareFlag);  // Send message
+  jC = String(jamCounter[0]) + "," + String(jamCounter[1]) + "," + String(jamCounter[2]) + "," + String(jamCounter[3]);
+  Serial.println(jC);
+// --------------------------------------------------------------- HTTP CALL START ---------------------------------------------------------------
+  http.begin(client, update_density);
+  int httpCode = http.GET();
+  String responseD = http.getString();
+
+  http.begin(client, update_jam_count);
+  httpCode = http.GET();
+  String responseJ = http.getString();
+// --------------------------------------------------------------- HTTP CALL END ---------------------------------------------------------------
+
+  Serial.println("Density Server Message Rec: " + responseD);
+  Serial.println("Jam Count Server Message Rec: " + responseJ);
   delay(1000);
 }
 
 int checkTraffic(int i) {
-  int j;
+  int j = 0;
   while (j < checkDuration) {
     // Clears the trigPin
     digitalWrite(trigPin[i], LOW);
@@ -88,10 +132,11 @@ int checkTraffic(int i) {
     Serial.print("Distance: ");
     Serial.println(distance[i]);
     if (distance[i] > minDistance) {
-      return 0;
+      return 2;
     }
     delay(1000);
     j++;
   }
+  jamCounter[i]++;
   return 1;
 }
